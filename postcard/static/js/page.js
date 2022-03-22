272d43bebe4a428f1bb50f6cfcb6cf3635033898
nataliia-pysanka <pysankanataliia@gmail.com>
2022-03-23 00:28:11 +0200

    css is ok

--- postcard/static/js/page.js
+++ postcard/static/js/page.js
@@ -1,49 +1,57 @@
-const left_box = document.getElementById('left-box')
-const right_box = document.getElementById('right-box')
+//const left_box = document.getElementById('left-box')
+//const right_box = document.getElementById('right-box')
 const dropArea = document.getElementById('slide')
+const header = document.getElementById('page-header')
 
 const button = document.getElementById("browse_btn");
 const input = document.getElementById("input");
-const dragText = document.getElementById("text");
+const text = document.getElementById("text");
+const dragText = document.getElementById("drop_text");
+
+const main = document.getElementById("main");
 let file;
+let dropAreaWidth;
+let dropAreaHeight;
 
 start();
 window.addEventListener('resize', start);
 
 function start(){
-    left_box.style.left = "0px"
-    right_box.style.left = `${document.documentElement.clientWidth - 250}px`
-    dropArea.style.left = "250px"
-    //dropArea.style.height = `${scr_height - 190}px`
+    dropArea.style.height = `${document.documentElement.clientHeight - header.getBoundingClientRect().height -40}px`
+    dropAreaHeight = dropArea.getBoundingClientRect().height
+    dropArea.style.width = `${(dropAreaHeight * 148)/ 106}px`
+    dropAreaWidth = dropArea.getBoundingClientRect().width
 }
 
 button.onclick = () => input.click();
 
 input.onchange = (e) => {
-  file = e.files[0];
+  file = e.target.files[0];
   dropArea.classList.add("active");
   showImage();
 };
 
-//on drag over
 dropArea.ondragover = (event) => {
   event.preventDefault();
   dropArea.classList.add("active");
-//  dragText.textContent = "Release to Upload File";
+  dragText.style.display = "none";
 };
 
-//on drag leave
+
 dropArea.ondragleave = () => {
   dropArea.classList.remove("active");
-//  dragText.textContent = "Drag & Drop to Upload File";
+  dragText.style.display = "block";
 };
 
-//on drop
 dropArea.ondrop = (event) => {
   event.preventDefault();
   file = event.dataTransfer.files[0];
   showImage();
 };
+
+function calcSize() {
+
+}
 
 showImage = () => {
   let fileType = file.type;
@@ -52,16 +60,17 @@ showImage = () => {
   if(validFiletypes.includes(fileType)){
     let fileReader = new FileReader();
     fileReader.readAsDataURL(file);
+
     fileReader.onload = ()=>{
         let fileURL = fileReader.result;
-        let image = `<img src="${fileURL}" alt="image">`;
-        dropArea.innerHTML = image;
+        dropArea.style.backgroundImage = `url(${fileURL})`;
+//        let image = `<img src="${fileURL}" alt="image" width="${dropAreaWidth}" height="${dropAreaHeight}">`;
+//        dropArea.innerHTML = image;
         }
     }
   else{
     alert("This is not an Image File!");
     dropArea.classList.remove("active");
-//    dragText.textContent = "Drag & Drop to Upload File";
   }
 }
 
